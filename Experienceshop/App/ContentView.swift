f94079adb2bc270d460cd840dcdd96462532ae71
Bijan Saud <90811605+BijanSaud1@users.noreply.github.com>
2025-01-03 20:34:56 -0600

    Initial Commit

---
+++ Experienceshop/App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Experienceshop
+//
+//  Created by Bijan Saud on 1/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
